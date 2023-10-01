adf08b9f9c08605d3a140e85260e01a5ce2b4e6b
ShozoKurie <141247924+ShozoKurie@users.noreply.github.com>
2023-10-02 04:13:19 +0900

    firebaseに関するid等を環境変数で実装

---
+++ src/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+console.log("API Key:", process.env.REACT_APP_FIREBASE_API_KEY);
+console.log("API Key:", process.env.REACT_APP_FIREBASE_AUTH_DOMAIN);
+
+const firebaseApp = initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+});
+export const storage = getStorage(firebaseApp);
+export const auth = getAuth(firebaseApp);
+export const db = getFirestore(firebaseApp);
